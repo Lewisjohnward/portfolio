b85d2502fb2de84755df647b6aff7636932c539e
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-01-28 19:55:34 +0000

    Add icons

---
+++ src/misc/icons.ts
@@ -0,0 +1,14 @@
+import { FaLinkedin, FaGithub, FaDocker, FaReact } from "react-icons/fa";
+import { SiGithubactions, SiNeovim, SiTailwindcss } from "react-icons/si";
+import { TiDocumentText } from "react-icons/ti";
+export {
+   TiDocumentText ,
+  FaLinkedin,
+  FaGithub,
+  FaDocker,
+  FaReact,
+  SiGithubactions,
+  SiNeovim,
+  SiTailwindcss,
+};
+
