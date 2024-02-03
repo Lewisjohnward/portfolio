d318912ab5c3c59b4942aeea619505375afd3e3c
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-02-03 17:02:41 +0000

    Create object that contains projects

---
+++ src/misc/projects.ts
@@ -0,0 +1,14 @@
+export const projects = {
+  clippingstocards: {
+    id: "clippingstocards",
+    name: "Clippings to Cards",
+    image: "clippingstocards.png",
+    details: "heres details for clippings to cards",
+  },
+  tabatatimer: {
+    id: "tabatatimer",
+    name: "Tabata Timer",
+    image: "tabata.png",
+    details: "Heres details for tabata timer",
+  },
+};
