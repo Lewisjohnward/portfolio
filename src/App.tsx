ddc771d3c2389cfe5df2617697dfdadaac36df70
lewis ward <lewisjohnward+git1@gmail.com>
2024-02-06 14:28:52 +0000

    Alter style

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import { Project } from "./pages/Project";
 function App() {
   return (
     <BrowserRouter>
-    <div className="text-sm md:text-base h-[100dvh] flex flex-col max-w-6xl m-auto px-4 md:py-4">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:project" element={<Project />} />
-        <Route path="/*" element={<div>not found!</div>} />
-      </Routes>
-    </div>
+      <div className="text-sm md:text-base h-[100dvh] flex flex-col max-w-6xl m-auto px-4 md:pt-14 md:pb-8">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:project" element={<Project />} />
+          <Route path="/*" element={<div>not found!</div>} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
