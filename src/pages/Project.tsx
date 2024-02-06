7af5e5eb8685176b9d23eba186d11aa81aaeea17
lewis ward <lewisjohnward+git1@gmail.com>
2024-02-06 14:37:03 +0000

    Add project details

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -12,19 +12,52 @@ export function Project() {
     if (!projects[project as keyof typeof projects]) navigate("/");
   }, [project, navigate]);
 
-  const { details, name, image } = projects[project as keyof typeof projects];
+  const { details, name, image, link } =
+    projects[project as keyof typeof projects];
 
   return (
-    <div>
-      <Link to="/" className="text-slate-800 px-2 py-1 bg-white rounded">
-        Home
-      </Link>
-      <h1>{name}</h1>
-      <div>{project}</div>
-      <div>{details}</div>
-      <img src={image} />
-      <h2>About</h2>
-      <h2>Techincal details</h2>
+    <div className="flex-grow flex flex-col justify-between">
+      <div>
+        <div className="flex justify-end">
+          <Link to="/" className="text-slate-800 px-2 py-1 bg-white rounded">
+            Back
+          </Link>
+        </div>
+        <h1 className="text-4xl">{name}</h1>
+        <div className="space-y-4">
+          <p>{details}</p>
+          <Link
+            to={link}
+            target="_blank"
+            className="inline-block px-2 py-1 rounded"
+          >
+            Visit the website
+          </Link>
+          <img src={image} />
+          <h2 className="text-2xl">About</h2>
+          <p className="text-white/60">
+            Project developed because creating Anki flashcards from the
+            clippings files produced by the Kindle when highlighting text is
+            laborious and time consume. It allows quick creation of a large
+            amount of cards
+          </p>
+          <h2 className="text-2xl">Technical details</h2>
+          <ul className="text-xl list-disc list-inside [&>*]:text-white/60 [&>*]:text-base ">
+            <li>React</li>
+            <li>Zustand</li>
+            <li>Rollup</li>
+            <li>ES6</li>
+            <li>TailwindCSS</li>
+            <li>HTML5</li>
+          </ul>
+        </div>
+      </div>
+      <div className="w-1/2 mx-auto">
+        <p className="text-white/60">
+          Thought a project was particularly interesting? Feel free to contact
+          me via email lewisjohnward@gmail.com
+        </p>
+      </div>
     </div>
   );
 }
